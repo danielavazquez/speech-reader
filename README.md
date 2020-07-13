6d2e13eee5f9c3eee45966ed46ce07131ec64d1f
Daniela Vazquez <daniela_056@hotmail.com>
2020-07-13 17:05:34 -0600

    Set up new environment, transfer HTML, CSS, and JS code from previous repository

---
+++ README.md
@@ -0,0 +1,16 @@
+# Giphy App
+
+## Description
+
+A speech text reader app that uses the 'Web Speech API' to communicate basic needs, feelings, and places via audio. The app also includes a toggle text box that can be used to write messages and have them read aloud. Speech synthesis is used due to support in all browsers.
+
+## Installation
+
+Via npm
+
+> npm install
+
+> gulp watch
+
+## Demo
+This app is very slow due to failure in parsing sourceMap.
---
+++ src/js/index.js
@@ -0,0 +1,151 @@
+const main = document.querySelector('main');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+
+const data = [
+  {
+    image: './img/drink.jpg',
+    text: "I'm Thirsty"
+  },
+  {
+    image: './img/food.jpg',
+    text: "I'm Hungry"
+  },
+  {
+    image: './img/tired.jpg',
+    text: "I'm Tired"
+  },
+  {
+    image: './img/hurt.jpg',
+    text: "I'm Hurt"
+  },
+  {
+    image: './img/happy.jpg',
+    text: "I'm Happy"
+  },
+  {
+    image: './img/angry.jpg',
+    text: "I'm Angry"
+  },
+  {
+    image: './img/sad.jpg',
+    text: "I'm Sad"
+  },
+  {
+    image: './img/scared.jpg',
+    text: "I'm Scared"
+  },
+  {
+    image: './img/outside.jpg',
+    text: 'I Want To Go Outside'
+  },
+  {
+    image: './img/home.jpg',
+    text: 'I Want To Go Home'
+  },
+  {
+    image: './img/school.jpg',
+    text: 'I Want To Go To School'
+  },
+  {
+    image: './img/grandma.jpg',
+    text: 'I Want To Go To Grandmas'
+  }
+];
+
+data.forEach(createBox);
+
+//*First bring in all DOM Elements, create data array with image and text properties
+//*Second data.forEach loop that takes in a function as a parameter createBox
+//*Third create the createBox function that takes in item as a parameter createBox will be called for each iteration
+//*Fourth all content is straight HTML except boxes, we use main tag and JavaScript to output images and text dynamically (appending each box from array)
+//*Fifth add toggle functionality toggleBtn eventListener when we toggle show class we will have 0px for x coordinate which makes box appear -800px makes it go above out of UI
+//*Sixth create empty voices data array and getVoices function so we have dro down function with voice changing capability
+
+//Create speech boxes
+//we destructure {image, text} because we need both and set = to item because item is an object with two properties
+//do this so we don't type item.image and item.text, can automatically start using below
+function createBox(item) {
+  const box = document.createElement('div');
+
+  const { image, text } = item;
+
+  box.classList.add('box');
+
+  box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    // Add active effect
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  });
+
+  main.appendChild(box);
+}
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
+
+// Store voices
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// Set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// Speak text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+// Set voice
+function setVoice(e) {
+  message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+// Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.toggle('show')
+);
+
+// Close button
+closeBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.remove('show')
+);
+
+// Change voice
+voicesSelect.addEventListener('change', setVoice);
+
+// Read text button
+readBtn.addEventListener('click', () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getVoices();
